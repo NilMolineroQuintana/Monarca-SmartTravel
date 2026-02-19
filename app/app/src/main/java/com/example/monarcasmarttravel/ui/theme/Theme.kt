ad9924993e5dbfbffe2d07cf8b780c638040d2ed
NilMolineroQuintana <nmq2@alumnes.udl.cat>
2026-02-19 19:51:38 +0100

    Clean up imports and code structure

--- app/app/src/main/java/com/example/monarcasmarttravel/ui/theme/Theme.kt
+++ app/app/src/main/java/com/example/monarcasmarttravel/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.monarcasmarttravel.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
