fb5ea85091d2b31c3a6ae1e5453cf5700f3b3f1d
Guillem Alcoverro <gal7@alumnes.udl.cat>
2026-03-06 23:03:27 +0100

    Changed app theme

--- app/app/src/main/java/com/example/monarcasmarttravel/ui/theme/Color.kt
+++ app/app/src/main/java/com/example/monarcasmarttravel/ui/theme/Color.kt
@@ -2,10 +2,25 @@ package com.example.monarcasmarttravel.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Colors primaris - Versió professional amb millor contrast
+val MagentaRose = Color(0xFFAF4B7C)         // Rosa magenta (color primari en mode clar)
+val RoyalPurple = Color(0xFF7A578D)         // Porpra reial (color secundari)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Colors d'accent - Càlids però llegibles
+val PeachyPink = Color(0xFFFFBFBB)          // Rosa préssec (color primari en mode fosc)
+val MauveAccent = Color(0xFF814674)         // Malva d'accent
+val SoftTaupe = Color(0xFFA89284)           // Taupe suau
+
+// Colors neutres - Alt contrast
+val PureWhite = Color(0xFFFFFFFF)           // Blanc pur
+val CreamWhite = Color(0xFFFBF8F6)          // Blanc crema
+val LightBeige = Color(0xFFF5F0ED)          // Beix clar
+val MediumGray = Color(0xFFA8A099)          // Gris mitjà
+val DarkCharcoal = Color(0xFF3D3D3D)        // Carbó fosc
+val RichBlack = Color(0xFF1F1F1F)           // Negre intens
+
+// Colors funcionals - Clars i professionals
+val ForestGreen = Color(0xFF2F7C4F)         // Verd bosc
+val GoldenAmber = Color(0xFFE59400)         // Ambre daurat
+val CrimsonRed = Color(0xFFD32F2F)          // Vermell carmesí
+val NavyBlue = Color(0xFF1565C0)            // Blau marí
