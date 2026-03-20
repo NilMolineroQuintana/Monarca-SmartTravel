5839ff0675d531ea72e78a5d8531715b53283aeb
Guillem Alcoverro <gal7@alumnes.udl.cat>
2026-03-20 19:23:43 +0100

    Add validation error cases to AppError

--- app/app/src/main/java/com/example/monarcasmarttravel/utils/AppError.kt
+++ app/app/src/main/java/com/example/monarcasmarttravel/utils/AppError.kt
@@ -8,6 +8,9 @@ enum class AppError(val code: Int, val stringRes: Int) {
     NON_EXISTING_TRIP(1,   R.string.error_non_existing_trip),
     NON_EXISTING_ITEM  (2,    R.string.error_item_not_found),
     ITEM_OUT_OF_RANGE(3,   R.string.error_item_out_of_range),
+    INVALID_TITLE   (4,    R.string.error_invalid_title),
+    INVALID_DESCRIPTION(5, R.string.error_invalid_description),
+    INVALID_DATE_RANGE(6,  R.string.error_invalid_date_range),
     NON_EXISTING_DATE(9998, R.string.error_non_existing_date),
     UNKNOWN         (9999, R.string.error_unknown);
 
