97d4fb1bc1197065ba12f56e3368f39e2a4d0ed0
NilMolineroQuintana <nmq2@alumnes.udl.cat>
2026-04-17 16:09:48 +0200

    Remove UserViewModel and clean up imports in MonarcaDatabase
    Deleted UserViewModel.kt and removed a duplicate TypeConverters import in MonarcaDatabase.kt.

--- app/app/src/main/java/com/example/monarcasmarttravel/data/MonarcaDatabase.kt
+++ app/app/src/main/java/com/example/monarcasmarttravel/data/MonarcaDatabase.kt
@@ -4,7 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.monarcasmarttravel.domain.model.ItineraryItem
-import androidx.room.TypeConverters
 import com.example.monarcasmarttravel.domain.model.AccesHistory
 import com.example.monarcasmarttravel.domain.model.User
 import com.example.monarcasmarttravel.utils.Converters
