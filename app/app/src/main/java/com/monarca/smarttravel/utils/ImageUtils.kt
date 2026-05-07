601c95431f13a8e8472ba4c9bda59b5459939c36
NilMolineroQuintana <nmq2@alumnes.udl.cat>
2026-05-07 14:54:48 +0200

    Update image storage location in `ImageUtils.kt`
    - Modify `copyUriToInternalFile` to save images in the external pictures directory instead of the internal files directory.

--- app/app/src/main/java/com/monarca/smarttravel/utils/ImageUtils.kt
+++ app/app/src/main/java/com/monarca/smarttravel/utils/ImageUtils.kt
@@ -2,6 +2,7 @@ package com.monarca.smarttravel.utils
 
 import android.content.Context
 import android.net.Uri
+import android.os.Environment
 import java.io.File
 import java.util.UUID
 
@@ -9,7 +10,8 @@ fun saveImageToInternalStorage(context: Context, uri: Uri): String? {
     return try {
         val inputStream = context.contentResolver.openInputStream(uri) ?: return null
         val fileName = "trip_img_${UUID.randomUUID()}.jpg"
-        val file = File(context.filesDir, fileName)
+        val dir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val file = File(dir, fileName)
         file.outputStream().use { output ->
             inputStream.copyTo(output)
         }
